7886467d83daa9724309ee3ba908542257609f75
AnYiteng <240938530@qq.com>
2022-04-11 20:59:18 +0800

    feat: prettier 规则更改

--- src/unauthenticated-app/index.tsx
+++ src/unauthenticated-app/index.tsx
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
-import LoginView from "./login";
-import RegisterView from "./register";
+import React, { useState } from 'react'
+import LoginView from './login'
+import RegisterView from './register'
 
 export const UnAuthenticated = () => {
-  const [isRegister, setIsRegister] = useState(false);
+  const [isRegister, setIsRegister] = useState(false)
 
   return (
     <div>
       {isRegister ? <LoginView /> : <RegisterView />}
       <button onClick={() => setIsRegister(!isRegister)}>
-        切换到{isRegister ? "注册" : "登录"}
+        切换到{isRegister ? '注册' : '登录'}
       </button>
     </div>
-  );
-};
+  )
+}
