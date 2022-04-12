166de93b56429e5025aec4aaf602ea2e2ec9c33f
AnYiteng <240938530@qq.com>
2022-04-12 20:36:35 +0800

    feat: 保持登录态

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -1,42 +1,58 @@
-import React, { useState, ReactNode } from "react";
-import { IUser } from "screens/projectList/index";
-import * as auth from "auth-provider";
+import React, { useState, ReactNode } from 'react'
+import { IUser } from 'screens/projectList/index'
+import * as auth from 'auth-provider'
+import { http } from 'utils/http'
+import { useMount } from 'utils'
 
 interface IAuthForm {
-  username: string;
-  password: string;
+  username: string
+  password: string
 }
 
 const AuthContext = React.createContext<
   | {
-      user: IUser | null;
-      login: (T: IAuthForm) => Promise<void>;
-      register: (T: IAuthForm) => Promise<void>;
-      logout: () => Promise<void>;
+      user: IUser | null
+      login: (T: IAuthForm) => Promise<void>
+      register: (T: IAuthForm) => Promise<void>
+      logout: () => Promise<void>
     }
   | undefined
->(undefined);
-AuthContext.displayName = "MyAuthContext";
+>(undefined)
+AuthContext.displayName = 'MyAuthContext'
+
+const bootstrapUser = async () => {
+  let user = null
+  const token = auth.getToken()
+  if (token) {
+    const data = await http('me', { token })
+    user = data.user
+  }
+  return user
+}
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [user, setUser] = useState<IUser | null>(null);
+  const [user, setUser] = useState<IUser | null>(null)
 
-  const login = (form: IAuthForm) => auth.login(form).then(setUser);
-  const register = (form: IAuthForm) => auth.register(form).then(setUser);
-  const logout = () => auth.logout().then(() => setUser(null));
+  const login = (form: IAuthForm) => auth.login(form).then(setUser)
+  const register = (form: IAuthForm) => auth.register(form).then(setUser)
+  const logout = () => auth.logout().then(() => setUser(null))
+
+  useMount(() => {
+    bootstrapUser().then(setUser)
+  })
 
   return (
     <AuthContext.Provider
       children={children}
       value={{ user, login, register, logout }}
     />
-  );
-};
+  )
+}
 
 export const useAuth = () => {
-  const context = React.useContext(AuthContext);
+  const context = React.useContext(AuthContext)
   if (!context) {
-    throw new Error("useAuth必须在AuthProvider中使用");
+    throw new Error('useAuth必须在AuthProvider中使用')
   }
-  return context;
-};
+  return context
+}
--- src/screens/projectList/index.tsx
+++ src/screens/projectList/index.tsx
@@ -1,57 +1,51 @@
-import React, { useState, useEffect } from "react";
-import { cleanObject, useMount, useDebounce } from "utils";
-import List from "./List";
-import SearchPanel from "./SearchPanel";
-import qs from "qs";
-
-const apiUrl = process.env.REACT_APP_API_URL;
+import React, { useState, useEffect } from 'react'
+import { cleanObject, useMount, useDebounce } from 'utils'
+import List from './List'
+import SearchPanel from './SearchPanel'
+import qs from 'qs'
+import { useHttp } from 'utils/http'
 
 export interface IUser {
-  id: string;
-  name: string;
-  email: string;
-  title: string;
-  organization: string;
-  token: string;
+  id: string
+  name: string
+  email: string
+  title: string
+  organization: string
+  token: string
 }
 
 export interface IList {
-  id: string;
-  name: string;
-  personId: string;
+  id: string
+  name: string
+  personId: string
 }
 
 export default function ProjectListScreen() {
-  const [users, setUsers] = useState<IUser[]>([]);
+  const [users, setUsers] = useState<IUser[]>([])
   const [params, setParams] = useState({
-    name: "",
-    personId: "",
-  });
-  const [list, setList] = useState<IList[]>([]);
-  const debounceParams = useDebounce(params, 500);
+    name: '',
+    personId: '',
+  })
+  const [list, setList] = useState<IList[]>([])
+  const debounceParams = useDebounce(params, 500)
+  const client = useHttp()
 
   useEffect(() => {
-    fetch(
-      `${apiUrl}/projects?${qs.stringify(cleanObject(debounceParams))}`
-    ).then(async (response) => {
-      if (response.ok) {
-        setList(await response.json());
-      }
-    });
-  }, [debounceParams]);
+    client('projects', { data: cleanObject(debounceParams) }).then((res) => {
+      setList(res)
+    })
+  }, [debounceParams])
 
   useMount(() => {
-    fetch(`${apiUrl}/users`).then(async (response) => {
-      if (response.ok) {
-        setUsers(await response.json());
-      }
-    });
-  });
+    client('users').then((res) => {
+      setUsers(res)
+    })
+  })
 
   return (
     <>
       <List users={users} list={list} />
       <SearchPanel users={users} params={params} setParams={setParams} />
     </>
-  );
+  )
 }
